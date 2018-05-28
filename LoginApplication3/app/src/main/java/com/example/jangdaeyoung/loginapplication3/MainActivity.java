f035e3a3d533308039d4233c0d1c4ddd9cbdf358
장대연-PC\Jang dae young <most25@naver.com>
2018-05-28 13:54:30 +0900

    안드로이드 게시판 서버연동

---
+++ LoginApplication3/app/src/main/java/com/example/jangdaeyoung/loginapplication3/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.jangdaeyoung.loginapplication3;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "loginapplication3_MainActivity";
+
+    private static final String TAG_JSON="page";
+    private static final String TAG_NUM = "num";
+    private static final String TAG_NICK = "nick";
+    private static final String TAG_HIT = "hit";
+    private static final String TAG_SUB = "subject";
+
+
+    private TextView mTextViewResult;
+    ArrayList<HashMap<String, String>> mArrayList;
+    ListView mlistView;
+    String mJsonString;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+        mlistView = (ListView) findViewById(R.id.noticeListView);
+        mArrayList = new ArrayList<>();
+
+        GetData task = new GetData();
+        task.execute("http://192.168.85.1/board_android.php");
+    }
+
+
+    private class GetData extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            mTextViewResult.setText(result);
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+
+                mTextViewResult.setText(errorString);
+            }
+            else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+
+    private void showResult(){
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String num = item.getString(TAG_NUM);
+                String nick = item.getString(TAG_NICK);
+                String hit = item.getString(TAG_HIT);
+                String sub = item.getString(TAG_SUB);
+
+                HashMap<String,String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_NUM, num);
+                hashMap.put(TAG_NICK, nick );
+                hashMap.put(TAG_HIT, hit);
+                hashMap.put(TAG_SUB, sub);
+
+                mArrayList.add(hashMap);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, mArrayList, R.layout.list_item,
+                    new String[]{TAG_NUM, TAG_NICK, TAG_HIT, TAG_SUB},
+                    new int[]{R.id.num, R.id.nick, R.id.hit, R.id.subject}
+            );
+
+            mlistView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+
+}
+
+
