8d2f7f4ac5137cd26490d8fc7843cc6598bce276
장대연-PC\Jang dae young <most25@naver.com>
2018-05-30 19:45:00 +0900

    수정해서 다시 올려드려요

--- LoginApplication3/app/src/main/java/com/example/jangdaeyoung/loginapplication3/LoginActivity.java
+++ LoginApplication3/app/src/main/java/com/example/jangdaeyoung/loginapplication3/LoginActivity.java
@@ -1,19 +1,84 @@
 package com.example.jangdaeyoung.loginapplication3;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
 
-
+    private AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText)findViewById(R.id.passwordText);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = idText.getText().toString();
+                String pw = passwordText.getText().toString();
+
+                Response.Listener<String> responseLister = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("success login")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                                finish();
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("Please check your ID and password.")
+                                        .setNegativeButton("Retry", null)
+                                        .create();
+                                dialog.show();
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(id, pw, responseLister);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
 
     }
+    protected void onStop(){
+        super.onStop();
+        if(dialog != null)
+        {
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
 }
+
+
---
+++ LoginApplication3/app/src/main/java/com/example/jangdaeyoung/loginapplication3/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.example.jangdaeyoung.loginapplication3;
+
+import android.support.annotation.StringRes;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.lang.reflect.Method;
+import java.net.ResponseCache;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+    final static private String URL = "http://172.30.83.127/login_android.php";
+    private Map<String, String> parameters;
+
+    public LoginRequest(String id, String pw, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("id",id);
+        parameters.put("pw",pw);
+    }
+    @Override
+    public Map<String, String> getParams() {return parameters;}
+}
